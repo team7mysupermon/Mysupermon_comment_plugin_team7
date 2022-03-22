7e42c0c96ab3ebc48978e34945b4ffe28d9d05bc
Sebastian Fugmann <sefu@itu.dk>
2022-03-22 16:36:57 +0100

    Added all methods to get the relevant information and put it into separate methods

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,36 +1,88 @@
+import netscape.javascript.JSObject;
+
 import java.net.InetAddress;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 
 public class Test {
     public static void main(String[] args) {
+        //To get package, class and method name
+        Object object = new Object(){};
+
+        String query = "Select * from difar";
+
+        System.out.println(combine(query, object));
+    }
+
+    public static String combine(String query, Object classObject) {
+        HashMap<String, Object> map = new HashMap<>();
+
+        map.putIfAbsent("METHODNAME", getMethodName(classObject));
+        map.putIfAbsent("HOST_NAME", getHostName());
+        map.putIfAbsent("LOCALHOST", getLocalHostName());
+        map.putIfAbsent("PACKAGENAME", getPackageName(classObject));
+        map.putIfAbsent("CLASSNAME", getClassName(classObject));
+        map.putIfAbsent("CURRENT_TIME", getTimeNow());
+        map.putIfAbsent("IP_ADDRESS", getIP());
+
+        return query + map.toString();
+    }
+
+    private static String getLocalHostName() {
+        return getHostName() + "/" + getIP();
+    }
+
+    private static String getHostName() {
+        String value = "";
+
+        try {
+            InetAddress inetAddress = InetAddress.getLocalHost();
+
+            value = inetAddress.getHostName();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return value;
+    }
+
+    private static String getIP() {
+        String value = "";
+
         try {
             //To get the IP address
             InetAddress inetAddress = InetAddress.getLocalHost();
 
             //Gets the IP address
-            String ipAddress = inetAddress.getHostAddress();
+            value = inetAddress.getHostAddress();
 
-            //To get package, class and method name
-            Object object = new Object(){};
-
-            //Gets method and class name
-            String methodName = object.getClass().getEnclosingMethod().getName();
-            String className = object.getClass().getName();
-
-            //The specific format the time should be printed in
-            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("uuuu/MM/dd HH:mm:ss");
-
-            //Gets the current time
-            LocalDateTime now = LocalDateTime.now();
-
-            //Prints out everything
-            System.out.println("IP address: " + ipAddress + "\n" + "Method name: " + methodName);
-            System.out.println("Class name: " + className);
-            System.out.println(dtf.format(now));
-
-        } catch (Exception e) {
+        } catch (Exception e){
             e.printStackTrace();
         }
+        return value;
+    }
+
+    private static String getTimeNow() {
+        //The specific format the time should be printed in
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("uuuu/MM/dd HH:mm:ss");
+
+        //Gets the current time
+        LocalDateTime now = LocalDateTime.now();
+
+        return dtf.format(now);
+    }
+
+    private static String getPackageName(Object classObject) {
+        return classObject.getClass().getPackage().getName();
+    }
+
+    private static String getClassName(Object classObject) {
+        return classObject.getClass().getName();
+    }
+
+    private static String getMethodName(Object classObject) {
+        return classObject.getClass().getEnclosingMethod().getName();
     }
 }
