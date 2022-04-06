00b168e7e70629996fba8fddce52ac4b0eacbf0e
aHjelmgaard <90194013+aHjelmgaard@users.noreply.github.com>
2022-04-06 14:43:55 +0200

    Feature/library GD-7. No Export. (#21)
    * Added the jdbc depencency to pom
    
    * Added extension to the Test.java class
    
    * Renamed class
    
    * Refactored helper methods
    Started work on testing helper methods and Jdbc methods
    Added extra imports for testing
    
    * Refactored tests to adhere to clean code principles. Problem with IP, Host and Localhost remains.
    
    * Fixed IP and Local host Mock - All tests now works as intended
    
    Created a new "custom" InetAddress and run the mock on it as base.
    All implemented tests work as intended.
    
    * Added test for 'AddSystemData' and continued refactoring.
    
    * Made testing fields private and final.
    
    * Deleted Program class and instance
    
    Co-authored-by: Sebastian Fugmann <sefu@itu.dk>
    Co-authored-by: Alexander Hjelmgaard <hjel@itu.dk>
    Co-authored-by: Alexander Schiller-Stefansson <alesc@itu.dk>

---
+++ src/main/java/MySuperMonNamedParameterJdbcTemplate.java
@@ -0,0 +1,27 @@
+import Util.CommentAssistant;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.lang.Nullable;
+
+import javax.sql.DataSource;
+
+public class MySuperMonNamedParameterJdbcTemplate extends NamedParameterJdbcTemplate {
+    public MySuperMonNamedParameterJdbcTemplate(DataSource dataSource) {
+        super(dataSource);
+    }
+
+    public static void main(String[] args) {
+        String query = "SELECT * FROM difar";
+
+        System.out.println(CommentAssistant.addSystemData(query));
+    }
+
+    @Nullable
+    @Override
+    public <T> T query(String sql, SqlParameterSource paramSource, ResultSetExtractor<T> rse) throws DataAccessException {
+        String value = CommentAssistant.addSystemData(sql);
+        return super.query(value, paramSource, rse);
+    }
+}
---
+++ src/test/java/UtilTests/CommentAssistantTests.java
@@ -0,0 +1,108 @@
+package UtilTests;
+
+import Util.CommentAssistant;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+
+import java.net.InetAddress;
+import java.time.*;
+import java.time.Month;
+
+public class CommentAssistantTests {
+    private static final String expectedHost = "testHost";
+    private static final String expectedIP = "1.2.3.4";
+
+    @BeforeAll
+    public static void setUp() {
+        // Creating mock for date time.
+        LocalDateTime dateTime = LocalDateTime.of(2015, Month.JULY, 29, 19, 30, 40);
+        mockStatic(LocalDateTime.class);
+        when(LocalDateTime.now()).thenReturn(dateTime);
+
+        // Creating mock for InetAddress.
+        try {
+            InetAddress address = InetAddress.getByName(expectedIP);
+            mockStatic(InetAddress.class);
+            when(InetAddress.getLocalHost()).thenReturn(address);
+            when(InetAddress.getLocalHost().getHostName()).thenReturn(expectedHost);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Calling class with test string.
+        CommentAssistant.addSystemData("test");
+    }
+
+    @Test
+    public void getLocalHostName_ReturnsHostNameAndIP() {
+        String expected = expectedHost + "/" + expectedIP;
+        String actual = CommentAssistant.getLocalHostName();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getHostName_ReturnsHostName(){
+        String expected = expectedHost;
+        String actual = CommentAssistant.getHostName();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getIP_ReturnsIpAddress(){
+        String expected = expectedIP;
+        String actual= CommentAssistant.getIP();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void getTimeNow_ReturnsCurrentTime() {
+        String expected = "2015/07/29 19:30:40";
+        String actual = CommentAssistant.getTime();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getMethodName_GivenMethodCall_ReturnsMethodName() {
+        String expected = "setUp";
+        String actual = CommentAssistant.getMethodName();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getClassName_GivenMethodHasClass_ReturnsClassName() {
+        String expected = "UtilTests.CommentAssistantTests";
+        String actual = CommentAssistant.getClassName();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getPackageName_GivenClassHasPackage_ReturnsPackageName(){
+        String expected = "UtilTests";
+        String actual = CommentAssistant.getPackageName();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getFinalString_GivenAddSystemDataWithParameterTest_ReturnsTestWithSystemData() {
+        String expected = "test/*{\"METHODNAME\":\"setUp\"," +
+                                "\"HOST_NAME\":\"" + expectedHost + "\"," +
+                                "\"LOCALHOST\":\"" + expectedHost + "/" + expectedIP + "\"," +
+                                "\"IP_ADDRESS\":\"" + expectedIP + "\"," +
+                                "\"CLASSNAME\":\"UtilTests.CommentAssistantTests\"," +
+                                "\"CURRENT_TIME\":\"2015/07/29 19:30:40\"," +
+                                "\"PACKAGENAME\":\"UtilTests\"}*/";
+        String actual = CommentAssistant.getFinalString();
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
