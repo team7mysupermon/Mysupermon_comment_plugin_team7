f0ccb41d3aba6561fedb4fd529a82687839dab4e
Sebastian Fugmann <sefu@itu.dk>
2022-04-14 15:24:10 +0200

    Added the remaining methods for the library

--- src/main/java/NamedJDBC/MySuperMonNamedParameterJdbcTemplate.java
+++ src/main/java/NamedJDBC/MySuperMonNamedParameterJdbcTemplate.java
@@ -3,19 +3,20 @@ package NamedJDBC;
 import Util.CommentAssistant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.PreparedStatementCallback;
-import org.springframework.jdbc.core.ResultSetExtractor;
-import org.springframework.jdbc.core.RowCallbackHandler;
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.*;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.ParsedSql;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.lang.Nullable;
 
 import javax.sql.DataSource;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 import java.util.logging.Logger;
+import java.util.stream.Stream;
 
 public class MySuperMonNamedParameterJdbcTemplate extends NamedParameterJdbcTemplate {
     private final CommentAssistant commentAssistant;
@@ -26,43 +27,49 @@ public class MySuperMonNamedParameterJdbcTemplate extends NamedParameterJdbcTemp
         this.commentAssistant = new CommentAssistant(Logger.getGlobal());
     }
 
-    @Nullable
+    @Autowired
+    public MySuperMonNamedParameterJdbcTemplate(JdbcOperations classicJdbcTemplate) {
+        super(classicJdbcTemplate);
+        this.commentAssistant = new CommentAssistant(Logger.getGlobal());
+    }
+
     @Override
+    @Nullable
     public <T> T execute(String sql, SqlParameterSource paramSource, PreparedStatementCallback<T> action) throws DataAccessException {
         String value = commentAssistant.addSystemData(sql);
         return super.execute(value, paramSource, action);
     }
 
-    @Nullable
     @Override
+    @Nullable
     public <T> T execute(String sql, Map<String,?> paramMap, PreparedStatementCallback<T> action) throws DataAccessException {
         String value = commentAssistant.addSystemData(sql);
         return super.execute(value, paramMap, action);
     }
 
-    @Nullable
     @Override
+    @Nullable
     public <T> T execute(String sql, PreparedStatementCallback<T> action) throws DataAccessException {
         String value = commentAssistant.addSystemData(sql);
         return super.execute(value, action);
     }
 
-    @Nullable
     @Override
+    @Nullable
     public <T> T query(String sql, SqlParameterSource paramSource, ResultSetExtractor<T> rse) throws DataAccessException {
         String value = commentAssistant.addSystemData(sql);
         return super.query(value, paramSource, rse);
     }
 
-    @Nullable
     @Override
+    @Nullable
     public <T> T query(String sql, Map<String,?> paramMap, ResultSetExtractor<T> rse) throws DataAccessException {
         String value = commentAssistant.addSystemData(sql);
         return super.query(value, paramMap, rse);
     }
 
-    @Nullable
     @Override
+    @Nullable
     public <T> T query(String sql, ResultSetExtractor<T> rse) throws DataAccessException {
         String value = commentAssistant.addSystemData(sql);
         return super.query(value, rse);
@@ -92,8 +99,8 @@ public class MySuperMonNamedParameterJdbcTemplate extends NamedParameterJdbcTemp
         return super.query(value, paramSource, rowMapper);
     }
 
-    @Nullable
     @Override
+    @Nullable
     public <T> List<T> query(String sql, Map<String,?> paramMap, RowMapper<T> rowMapper) throws DataAccessException {
         String value = commentAssistant.addSystemData(sql);
         return super.query(value, paramMap, rowMapper);
@@ -103,6 +110,94 @@ public class MySuperMonNamedParameterJdbcTemplate extends NamedParameterJdbcTemp
     public <T> List<T> query(String sql, RowMapper<T> rowMapper) throws DataAccessException {
         String value = commentAssistant.addSystemData(sql);
         return super.query(value, rowMapper);
+    }
+
+    @Override
+    public <T> Stream<T> queryForStream(String sql, SqlParameterSource paramSource, RowMapper<T> rowMapper) throws DataAccessException {
+        String value = commentAssistant.addSystemData(sql);
+        return super.queryForStream(value, paramSource, rowMapper);
+    }
+
+    @Override
+    public <T> Stream<T> queryForStream(String sql, Map<String,?> paramMap, RowMapper<T> rowMapper) throws DataAccessException {
+        String value = commentAssistant.addSystemData(sql);
+        return super.queryForStream(value, paramMap, rowMapper);
+    }
+
+    @Override
+    @Nullable
+    public <T> T queryForObject(String sql, SqlParameterSource paramSource, RowMapper<T> rowMapper) throws DataAccessException {
+        String value = commentAssistant.addSystemData(sql);
+        return super.queryForObject(value, paramSource, rowMapper);
+    }
+
+    @Override
+    @Nullable
+    public <T> T queryForObject(String sql, Map<String,?> paramMap, RowMapper<T> rowMapper) throws DataAccessException {
+        String value = commentAssistant.addSystemData(sql);
+        return super.queryForObject(value, paramMap, rowMapper);
+    }
+
+    @Override
+    @Nullable
+    public <T> T queryForObject(String sql, SqlParameterSource paramSource, Class<T> requiredType) throws DataAccessException {
+        String value = commentAssistant.addSystemData(sql);
+        return super.queryForObject(value, paramSource, requiredType);
+    }
+
+    @Override
+    @Nullable
+    public <T> T queryForObject(String sql, Map<String,?> paramMap, Class<T> requiredType) throws DataAccessException {
+        String value = commentAssistant.addSystemData(sql);
+        return super.queryForObject(value, paramMap, requiredType);
+    }
+
+    @Override
+    public Map<String,Object> queryForMap(String sql, SqlParameterSource paramSource) throws DataAccessException {
+        String value = commentAssistant.addSystemData(sql);
+        return super.queryForMap(value, paramSource);
+    }
+
+    @Override
+    public Map<String,Object> queryForMap(String sql, Map<String,?> paramMap) throws DataAccessException {
+        String value = commentAssistant.addSystemData(sql);
+        return super.queryForMap(value, paramMap);
+    }
+
+    @Override
+    public <T> List<T> queryForList(String sql, SqlParameterSource paramSource, Class<T> elementType) throws DataAccessException {
+        String value = commentAssistant.addSystemData(sql);
+        return super.queryForList(value, paramSource, elementType);
+    }
+
+    @Override
+    public <T> List<T> queryForList(String sql, Map<String,?> paramMap, Class<T> elementType) throws DataAccessException {
+        String value = commentAssistant.addSystemData(sql);
+        return super.queryForList(value, paramMap, elementType);
+    }
+
+    @Override
+    public List<Map<String,Object>> queryForList(String sql, SqlParameterSource paramSource) throws DataAccessException {
+        String value = commentAssistant.addSystemData(sql);
+        return super.queryForList(value, paramSource);
+    }
+
+    @Override
+    public List<Map<String,Object>> queryForList(String sql, Map<String,?> paramMap) throws DataAccessException {
+        String value = commentAssistant.addSystemData(sql);
+        return super.queryForList(value, paramMap);
+    }
+
+    @Override
+    public SqlRowSet queryForRowSet(String sql, SqlParameterSource paramSource) throws DataAccessException {
+        String value = commentAssistant.addSystemData(sql);
+        return super.queryForRowSet(value, paramSource);
+    }
+
+    @Override
+    public SqlRowSet queryForRowSet(String sql, Map<String,?> paramMap) throws DataAccessException {
+        String value = commentAssistant.addSystemData(sql);
+        return super.queryForRowSet(value, paramMap);
     }
 
     @Override
@@ -132,5 +227,44 @@ public class MySuperMonNamedParameterJdbcTemplate extends NamedParameterJdbcTemp
             throws DataAccessException {
         String value = commentAssistant.addSystemData(sql);
         return super.update(value, paramSource, generatedKeyHolder, keyColumnNames);
+    }
+
+    @Override
+    public int[] batchUpdate(String sql, Map<String,?>[] batchValues) {
+        String value = commentAssistant.addSystemData(sql);
+        return super.batchUpdate(value, batchValues);
+    }
+
+    @Override
+    public int[] batchUpdate(String sql, SqlParameterSource[] batchArgs) {
+        String value = commentAssistant.addSystemData(sql);
+        return super.batchUpdate(value, batchArgs);
+    }
+
+    @Override
+    protected PreparedStatementCreator getPreparedStatementCreator(String sql, SqlParameterSource paramSource) {
+        String value = commentAssistant.addSystemData(sql);
+        return super.getPreparedStatementCreator(value, paramSource);
+    }
+
+    @Override
+    protected PreparedStatementCreator getPreparedStatementCreator(String sql,
+                                                                   SqlParameterSource paramSource,
+                                                                   @Nullable
+                                                                               Consumer<PreparedStatementCreatorFactory> customizer) {
+        String value = commentAssistant.addSystemData(sql);
+        return super.getPreparedStatementCreator(value, paramSource, customizer);
+    }
+
+    @Override
+    protected ParsedSql getParsedSql(String sql) {
+        String value = commentAssistant.addSystemData(sql);
+        return super.getParsedSql(value);
+    }
+
+    @Override
+    protected PreparedStatementCreatorFactory getPreparedStatementCreatorFactory(ParsedSql parsedSql, SqlParameterSource paramSource) {
+        ParsedSql value = getParsedSql(parsedSql.toString());
+        return super.getPreparedStatementCreatorFactory(value, paramSource);
     }
 }
